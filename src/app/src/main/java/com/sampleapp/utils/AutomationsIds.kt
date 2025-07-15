b7779d19d14c31d3cb40a8f7e221b4d009c24a21
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 18:57:23 +0530

    Create AutomationsIds.kt

---
+++ src/app/src/main/java/com/sampleapp/utils/AutomationsIds.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2025 by Sample App. All Rights Reserved.
+ */
+
+package com.sampleapp.utils
+
+object AutomationsIds {
+    object WelcomeScreen {
+        const val WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header"
+        const val WELCOME_SCREEN_TEXT_DESCRIPTION = "welcome_screen_text_description"
+        const val WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue"
+    }
+
+    object LoginScreen {
+            const val LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header"
+            const val LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email"
+            const val LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password"
+            const val LOGIN_SCREEN_BUTTON_SUBMIT = "login_screen_button_submit"
+    }
+}
