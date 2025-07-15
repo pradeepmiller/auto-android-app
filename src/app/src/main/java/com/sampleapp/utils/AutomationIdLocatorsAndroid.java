6fe2e9227c00983af1908cff6abe9078967e3d35
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 14:41:01 +0000

    Convert AutomationsIds.kt to AutomationIdLocatorsAndroid.java using Python script

--- src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
+++ src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
@@ -4,21 +4,22 @@
 public class AutomationIdLocatorsForAndroid {
 
     public static class WelcomeScreen {
+        public static final String WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue";
         public static final String WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header";
         public static final String WELCOME_SCREEN_TEXT_DESCRIPTION = "welcome_screen_text_description";
-        public static final String WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue";
     }
 
     public static class LoginScreen {
-        public static final String LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header";
         public static final String LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email";
+        public static final String LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header";
         public static final String LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password";
         public static final String LOGIN_SCREEN_BUTTON_SUBMIT = "login_screen_button_submit";
     }
 
     public static class HomeScreen {
-        public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
         public static final String HOME_SCREEN_TEXT_DESCRIPTION = "home_screen_text_description";
+        public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
+        public static final String HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu";
     }
 
 }
