01da4c1be1c5534d435b1cff651bda3adffe1c66
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-15 16:55:56 +0000

    Convert AutomationsIds.kt to AutomationIdLocatorsAndroid.java using Python script

--- src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
+++ src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
@@ -5,8 +5,8 @@ public class AutomationIdLocatorsForAndroid {
 
     public static class WelcomeScreen {
         public static final String WELCOME_SCREEN_BUTTON_CONTINUE = "welcome_screen_button_continue";
-        public static final String WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header";
         public static final String WELCOME_SCREEN_TEXT_DESCRIPTION = "welcome_screen_text_description";
+        public static final String WELCOME_SCREEN_TEXT_HEADER = "welcome_screen_text_header";
     }
 
     public static class LoginScreen {
@@ -18,8 +18,13 @@ public class AutomationIdLocatorsForAndroid {
 
     public static class HomeScreen {
         public static final String HOME_SCREEN_TEXT_DESCRIPTION = "home_screen_text_description";
-        public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
         public static final String HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu";
+        public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
+    }
+
+    public static class Common {
+        public static final String BUTTON_BACK = "button_back";
+        public static final String BUTTON_CLOSE = "button_close";
     }
 
 }
