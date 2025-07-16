d762fd25023320cd0b75734e7215e12edcf83777
pradeepmiller <47136634+pradeepmiller@users.noreply.github.com>
2025-07-16 10:43:44 +0000

    Convert AutomationsIds.kt to AutomationIdLocatorsAndroid.java using Python script

--- src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
+++ src/app/src/main/java/com/sampleapp/utils/AutomationIdLocatorsAndroid.java
@@ -10,25 +10,21 @@ public class AutomationIdLocatorsForAndroid {
     }
 
     public static class LoginScreen {
-        public static final String LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email";
         public static final String LOGIN_SCREEN_TEXT_HEADER = "login_screen_text_header";
-        public static final String LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password";
         public static final String LOGIN_SCREEN_BUTTON_SUBMIT = "login_screen_button_submit";
+        public static final String LOGIN_SCREEN_INPUT_EMAIL = "login_screen_input_email";
+        public static final String LOGIN_SCREEN_INPUT_PASSWORD = "login_screen_input_password";
     }
 
     public static class HomeScreen {
         public static final String HOME_SCREEN_TEXT_DESCRIPTION = "home_screen_text_description";
-        public static final String HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu";
         public static final String HOME_SCREEN_TEXT_HEADER = "home_screen_text_header";
+        public static final String HOME_SCREEN_BUTTON_MENU = "home_screen_button_menu";
     }
 
     public static class Common {
-        public static final String BUTTON_BACK = "button_back";
         public static final String BUTTON_CLOSE = "button_close";
-    }
-
-    public static class MenuScreen {
-        public static final String MENU_SCREEN_ICON_PROFILE = "menu_screen_icon_profile";
+        public static final String BUTTON_BACK = "button_back";
     }
 
 }
